087a8564679bbff6087c289a35415b8d6a3aad58
Thomas Aggerholm <thomasaggerholm@hotmail.com>
2020-03-29 15:16:53 +0200

    Changes

--- scripts/app.js
+++ scripts/app.js
@@ -2,17 +2,18 @@ const backgroundTextLetters = document.querySelectorAll('.js-homepage-bg-text sp
 const homepageTitleSmall = document.querySelector('.homepage__title--small')
 const homepageTitleBig = document.querySelector('.homepage__title--big')
 
-let delay = 0.2
+let duration = 0.5
 
 function bgTextFadeIn() {
     backgroundTextLetters.forEach(letter => {
         letter.classList.add('animated')
-        letter.style.transitionDelay = `${delay}s`
-        delay += 0.2
+        letter.style.transitionDuration = `${duration}s`
+        duration += 0.2
     })
 
     homepageTitleSmall.classList.add('animated')
     homepageTitleBig.classList.add('animated')
+    console.log('Working')
 }
 
 bgTextFadeIn()
